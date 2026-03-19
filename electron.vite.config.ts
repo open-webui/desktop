19e264f494184f28ece8d8673be36bc290910117
Timothy Jaeryang Baek <tim@openwebui.com>
2026-03-18 19:17:58 -0500

    refac

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'electron-vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from '@tailwindcss/vite'
 
+if (process.platform === 'linux') {
+  process.env.ELECTRON_DISABLE_SANDBOX = '1'
+}
+
 export default defineConfig({
   main: {},
   preload: {},
